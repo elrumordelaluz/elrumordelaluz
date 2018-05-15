1b91a2034363a8014222df18718b1ae74e194499
Lionel T <elrumordelaluz@hotmail.com>
2018-05-16 00:07:36 +0200

    Switch favicon based on tab status

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,28 +1,61 @@
-import React from 'react'
+import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
 
 import Lamp from '../components/lamp'
 import Social from '../components/social'
+import lampOn from './lamp_on.png'
+import lampOff from './lamp_off.png'
+
 import './index.css'
 
-const Layout = ({ children, data }) => (
-  <div>
-    <Helmet
-      title={data.site.siteMetadata.title}
-      meta={[
-        { name: 'description', content: 'Sample' },
-        { name: 'keywords', content: 'sample, something' },
-      ]}
-    />
-    <Lamp />
-    {children()}
-    <Social />
-  </div>
-)
+class Layout extends Component {
+  static propTypes = {
+    children: PropTypes.func,
+  }
 
-Layout.propTypes = {
-  children: PropTypes.func,
+  componentDidMount() {
+    window.addEventListener('focus', this.setActiveIcon)
+    window.addEventListener('blur', this.setUnactiveIcon)
+  }
+
+  setActiveIcon = () => {
+    var link =
+      document.querySelector("link[rel*='icon']") ||
+      document.createElement('link')
+    link.type = 'image/x-icon'
+    link.rel = 'shortcut icon'
+    link.href = lampOn
+    document.getElementsByTagName('head')[0].appendChild(link)
+  }
+
+  setUnactiveIcon = () => {
+    var link =
+      document.querySelector("link[rel*='icon']") ||
+      document.createElement('link')
+    link.type = 'image/x-icon'
+    link.rel = 'shortcut icon'
+    link.href = lampOff
+    document.getElementsByTagName('head')[0].appendChild(link)
+  }
+
+  render() {
+    const { children, data } = this.props
+
+    return (
+      <div>
+        <Helmet>
+          <title>{data.site.siteMetadata.title}</title>
+          <meta name="description" content="Lionel Tzatzkin" />
+          <link rel="icon" type="image/png" sizes="192x192" href={lampOn} />
+        </Helmet>
+
+        <Lamp />
+        {children()}
+        <Social />
+      </div>
+    )
+  }
 }
 
 export default Layout
