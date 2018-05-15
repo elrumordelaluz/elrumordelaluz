d5692008e3678fd517351affa4756bc73a675b70
Lionel T <elrumordelaluz@hotmail.com>
2018-05-15 09:50:22 +0200

    Remove the now ignored directories "public" and ".cache"

--- .cache/layouts/index.js
+++
@@ -1,7 +0,0 @@
-
-  import React from "react"
-  import Component from "/Users/lio/Sites/elrumordelaluz/src/layouts/index.js"
-  import data from "/Users/lio/Sites/elrumordelaluz/.cache/json/layout-index.json"
-
-  export default (props) => <Component {...props} {...data} />
-  
