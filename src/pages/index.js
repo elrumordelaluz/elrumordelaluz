3cf1ca19d570daf70bb329c8bc07f43e30394a5e
Lionel T <elrumordelaluz@hotmail.com>
2018-05-22 16:37:48 +0200

    Switch to CSS Modules

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,9 @@
 import React from 'react'
 import Link from 'gatsby-link'
+import styles from './index.module.css'
 
 const IndexPage = () => (
-  <h1 className="title">
+  <h1 className={`${styles.title} title`}>
     elrumordelaluz<br />
     <small>Lionel Tzatzkin</small>
   </h1>
