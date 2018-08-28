2bc9500a2007150fabcea0008a844091edc967c4
Lionel T <elrumordelaluz@hotmail.com>
2018-08-28 15:45:52 +0200

    Update index

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,16 +4,30 @@ import styles from './index.module.css'
 
 const IndexPage = () => (
   <Layout>
-    <div className={styles.grid}>
-      <div className={styles.a}>
+    <div className={styles.wrapper}>
+      <div className={styles.intro}>
+        <p>Hola, my name is Lionel Tzatzkin.</p>
         <p>
-          Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam
-          nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat
-          volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation
-          ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.{' '}
+          I am a <span className={styles.highlight}>fullstack developer</span>{' '}
+          who studied for{' '}
+          <span className={styles.highlight}>graphic designer</span> and worked
+          as <span className={styles.highlight}>art director</span> before write
+          code all day.
+        </p>
+        <p>
+          I'm currently working for{' '}
+          <a className={styles.link} href="https://pixeden.com">
+            Pixeden
+          </a>
+          , pushing stuff forward into new technologies and creating awesome new
+          tools, like{' '}
+          <a className={styles.link} href="https://orioniconlibrary.com">
+            Orion Icon Library
+          </a>
+          .
         </p>
       </div>
-      <div className={styles.b}>
+      <div className={styles.quotes}>
         <p>
           Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam
           nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat
@@ -29,7 +43,7 @@ const IndexPage = () => (
           volutpat.
         </p>
       </div>
-      <div className={styles.c}>
+      <div className={styles.copy}>
         Idea, <br />
         Design, <br />
         Code.
