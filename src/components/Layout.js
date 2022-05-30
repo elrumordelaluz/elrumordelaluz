29e45248000087295e93c25a37dac2e1fe5decff
Lionel <elrumordelaluz@hotmail.com>
2022-05-30 14:44:21 +0200

    Update name. Update dependencies

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { useEffect } from 'react'
 import Helmet from 'react-helmet'
 import { StaticQuery, graphql } from 'gatsby'
 
@@ -8,64 +8,60 @@ import iconInactive from './io_icn_2.png'
 import './fonts.css'
 import './reboot.css'
 
-class Layout extends Component {
-  componentDidMount() {
-    window.addEventListener('focus', this.setActiveIcon)
-    window.addEventListener('blur', this.setUnactiveIcon)
-  }
+function Layout({ children }) {
+  useEffect(() => {
+    function setActiveIcon() {
+      var link =
+        document.querySelector("link[rel*='icon']") ||
+        document.createElement('link')
+      link.type = 'image/x-icon'
+      link.rel = 'shortcut icon'
+      link.href = iconActive
+      document.getElementsByTagName('head')[0].appendChild(link)
+    }
 
-  setActiveIcon = () => {
-    var link =
-      document.querySelector("link[rel*='icon']") ||
-      document.createElement('link')
-    link.type = 'image/x-icon'
-    link.rel = 'shortcut icon'
-    link.href = iconActive
-    document.getElementsByTagName('head')[0].appendChild(link)
-  }
+    function setUnactiveIcon() {
+      var link =
+        document.querySelector("link[rel*='icon']") ||
+        document.createElement('link')
+      link.type = 'image/x-icon'
+      link.rel = 'shortcut icon'
+      link.href = iconInactive
+      document.getElementsByTagName('head')[0].appendChild(link)
+    }
 
-  setUnactiveIcon = () => {
-    var link =
-      document.querySelector("link[rel*='icon']") ||
-      document.createElement('link')
-    link.type = 'image/x-icon'
-    link.rel = 'shortcut icon'
-    link.href = iconInactive
-    document.getElementsByTagName('head')[0].appendChild(link)
-  }
+    window.addEventListener('focus', setActiveIcon)
+    window.addEventListener('blur', setUnactiveIcon)
+  }, [])
 
-  render() {
-    const { children } = this.props
-
-    return (
-      <StaticQuery
-        query={graphql`
-          query SiteTitleQuery {
-            site {
-              siteMetadata {
-                title
-              }
+  return (
+    <StaticQuery
+      query={graphql`
+        query SiteTitleQuery {
+          site {
+            siteMetadata {
+              title
             }
           }
-        `}
-        render={data => (
-          <>
-            <Helmet>
-              <title>{data.site.siteMetadata.title}</title>
-              <meta name="description" content="Lionel Tzatzkin" />
-              <link
-                rel="icon"
-                type="image/png"
-                sizes="192x192"
-                href={iconActive}
-              />
-            </Helmet>
-            {children}
-          </>
-        )}
-      />
-    )
-  }
+        }
+      `}
+      render={(data) => (
+        <>
+          <Helmet>
+            <title>{data.site.siteMetadata.title}</title>
+            <meta name="description" content="Lionel Tzatzkin" />
+            <link
+              rel="icon"
+              type="image/png"
+              sizes="192x192"
+              href={iconActive}
+            />
+          </Helmet>
+          {children}
+        </>
+      )}
+    />
+  )
 }
 
 export default Layout
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,123 +2,130 @@ import React from 'react'
 import Layout from '../components/Layout'
 import * as styles from './index.module.css'
 
-const IndexPage = () => (
-  <Layout>
-    <div className={styles.wrapper}>
-      <div className={styles.intro}>
-        <p>
-          Hola. My name is <span className={styles.highlight}>Lionel</span>.
-        </p>
-        <p>
-          I am a <span className={styles.highlight}>Front End Developer</span>{' '}
-          who started as{' '}
-          <span className={styles.highlight}>Graphic Designer</span>, worked as{' '}
-          <span className={styles.highlight}>Art Director</span> and found a
-          passion writing code.
-        </p>
-        <p className={styles.raw}>
-          I am currently working at{' '}
-          <a className={styles.link} href="https://pixeden.com">
-            Pixeden
-          </a>
-          , pushing stuff forward into new technologies/stacks, creating <a className={styles.link} href="https://orioniconlibrary.com">
-            awesome
-          </a>{' '}
-           <a className={styles.link} href="https://www.pixeden.com/music">
-            tools
-          </a>
-          
-          , closing the gap between{' '}
-          <span className={styles.highlight}>Designer</span> and{' '}
-          <span className={styles.highlight}>Developer</span> .
-        </p>
-        <p className={styles.raw}>
-          I mainly work with{' '}
-          <span className={styles.highlight}>JavaScript</span>, mostly with{' '}
-          <span className={styles.highlight}>React.js</span> but I am passionate
-          about <span className={styles.highlight}>SVG</span> and{' '}
-          <span className={styles.highlight}>CSS</span> as well.
-        </p>
-        <p className={styles.raw}>
-          I love the idea behind{' '}
-          <span className={styles.highlight}>Open Source</span>, so I create,
-          collaborate and mantain{' '}
-          <a className={styles.link} href="https://github.com/elrumordelaluz">
-            several open-source modules
-          </a>
-          . In general they born as a specific need working on something, and
-          then becomes useful to other people.
-        </p>
-      </div>
+export default function IndexPage() {
+  return (
+    <Layout>
+      <div className={styles.wrapper}>
+        <div className={styles.intro}>
+          <p>
+            Hola. My name is{' '}
+            <span className={styles.highlight}>Lionel Tzatzkin</span>.
+          </p>
+          <p>
+            I am a <span className={styles.highlight}>Front End Developer</span>{' '}
+            who started as{' '}
+            <span className={styles.highlight}>Graphic Designer</span>, worked
+            as <span className={styles.highlight}>Art Director</span> and found
+            a passion writing code.
+          </p>
+          <p className={styles.raw}>
+            I am currently working at{' '}
+            <a className={styles.link} href="https://pixeden.com">
+              Pixeden
+            </a>
+            , pushing stuff forward into new technologies/stacks, creating{' '}
+            <a className={styles.link} href="https://orioniconlibrary.com">
+              awesome
+            </a>{' '}
+            <a className={styles.link} href="https://www.pixeden.com/music">
+              tools
+            </a>
+            , closing the gap between{' '}
+            <span className={styles.highlight}>Designer</span> and{' '}
+            <span className={styles.highlight}>Developer</span> .
+          </p>
+          <p className={styles.raw}>
+            I mainly work with{' '}
+            <span className={styles.highlight}>JavaScript</span>, mostly with{' '}
+            <span className={styles.highlight}>React.js</span> but I am
+            passionate about <span className={styles.highlight}>SVG</span> and{' '}
+            <span className={styles.highlight}>CSS</span> as well.
+          </p>
+          <p className={styles.raw}>
+            I love the idea behind{' '}
+            <span className={styles.highlight}>Open Source</span>, so I create,
+            collaborate and mantain{' '}
+            <a className={styles.link} href="https://github.com/elrumordelaluz">
+              several open-source modules
+            </a>
+            . In general they born as a specific need working on something, and
+            then becomes useful to other people.
+          </p>
+        </div>
 
-      <div className={styles.quotes}>
-        <p>
-          You could find me on{' '}
-          <a className={styles.link} href="https://twitter.com/elrumordelaluz">
-            Twitter
-          </a>{' '}
-          or find stuff I made on{' '}
-          <a className={styles.link} href="https://github.com/elrumordelaluz">
-            Github
-          </a>
-          ,{' '}
-          <a className={styles.link} href="https://codepen.io/elrumordelaluz/">
-            Codepen
-          </a>
-          ,{' '}
-          <a
-            className={styles.link}
-            href="https://dribbble.com/elrumordelaluz/"
-          >
-            Dribbble
-          </a>{' '}
-          and{' '}
-          <a
-            className={styles.link}
-            href="https://www.behance.net/elrumordelaluz"
-          >
-            Behance
-          </a>
-          . Otherwise drop me an{' '}
-          <a className={styles.link} href="mailto:elrumordelaluz@gmail.com">
-            Email
-          </a>
-          .
-        </p>
-        <blockquote className={styles.quote}>
-          “Simplicity is about subtracting the obvious and adding the
-          meaningful.”
-          <footer>
-            — <cite>John Maeda</cite>
-          </footer>
-        </blockquote>
-        <p>
-          I am a{' '}
-          <a
-            className={styles.link}
-            href="https://en.wikipedia.org/wiki/Mate_(drink)"
-          >
-            mate
-          </a>{' '}
-          drinker (yup! I'm from Buenos Aires), living in a small hamlet in
-          Italy, near a lake and in front lovely horses. Play{' '}
-          <a
-            className={styles.link}
-            href="https://en.wikipedia.org/wiki/Pandeiro"
-          >
-            pandeiro
-          </a>{' '}
-          and observe the deep sky are my hobbies.
-        </p>
-      </div>
+        <div className={styles.quotes}>
+          <p>
+            You could find me on{' '}
+            <a
+              className={styles.link}
+              href="https://twitter.com/elrumordelaluz"
+            >
+              Twitter
+            </a>{' '}
+            or find stuff I made on{' '}
+            <a className={styles.link} href="https://github.com/elrumordelaluz">
+              Github
+            </a>
+            ,{' '}
+            <a
+              className={styles.link}
+              href="https://codepen.io/elrumordelaluz/"
+            >
+              Codepen
+            </a>
+            ,{' '}
+            <a
+              className={styles.link}
+              href="https://dribbble.com/elrumordelaluz/"
+            >
+              Dribbble
+            </a>{' '}
+            and{' '}
+            <a
+              className={styles.link}
+              href="https://www.behance.net/elrumordelaluz"
+            >
+              Behance
+            </a>
+            . Otherwise drop me an{' '}
+            <a className={styles.link} href="mailto:elrumordelaluz@gmail.com">
+              Email
+            </a>
+            .
+          </p>
+          <blockquote className={styles.quote}>
+            “Simplicity is about subtracting the obvious and adding the
+            meaningful.”
+            <footer>
+              — <cite>John Maeda</cite>
+            </footer>
+          </blockquote>
+          <p>
+            I am a{' '}
+            <a
+              className={styles.link}
+              href="https://en.wikipedia.org/wiki/Mate_(drink)"
+            >
+              mate
+            </a>{' '}
+            drinker (yup! I'm from Buenos Aires), living in a small hamlet in
+            Italy, near a lake and in front lovely horses. Play{' '}
+            <a
+              className={styles.link}
+              href="https://en.wikipedia.org/wiki/Pandeiro"
+            >
+              pandeiro
+            </a>{' '}
+            and observe the deep sky are my hobbies.
+          </p>
+        </div>
 
-      <div className={styles.copy}>
-        Idea, <br />
-        Design, <br />
-        Code.
+        <div className={styles.copy}>
+          Idea, <br />
+          Design, <br />
+          Code.
+        </div>
       </div>
-    </div>
-  </Layout>
-)
-
-export default IndexPage
+    </Layout>
+  )
+}
